cb797cb2e0023fc1b8efe67b053257491c0dcfec
Rafael Maia <rafael.augusto.fm@outlook.com>
2022-08-04 19:18:34 -0300

    Finishing challenge middlewares code

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.header
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User does not exist!" })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({ error: "User todos limit has been reached" })
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.header
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User does not exist!" })
+  }
+
+  const { id } = request.params
+
+  if (!validate(id)) {
+    return response.status(400).json({ error: "Todo id is not valid!" })
+  }
+
+  const todo = user.todos.find(todo => todo.id = id)
+  if (!todo) {
+    return response.status(404).json({ error: "User todo does not exist!" })
+  }
+
+  request.user = user
+  request.todo = todo
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { username } = request.params
+
+  const user = users.find(user => user.username === username)
+  if (!user) {
+    return response.status(404).json({ error: "User does not exist!" })
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
